093e8aca78da300a59e07bd0b9fa613819da2643
1442413269 <1442413269@qq.com>
2020-04-23 11:46:47 +0800

    修改

--- app/src/main/java/com/example/myapplicationp/MainActivity.java
+++ app/src/main/java/com/example/myapplicationp/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.myapplicationp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t1=(TextView)findViewById(R.id.textView);
+        t1.setText("Hello Wrold!");
     }
 }
